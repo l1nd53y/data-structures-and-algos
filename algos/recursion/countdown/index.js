bae1dc515c53bb9304b2ad03c77222bf329251a7
Lindsey Trujillo <lindsey.trujillo@verizon.com>
2022-03-10 15:26:03 -0600

    countdown progress

--- algos/recursion/countdown/index.js
+++ algos/recursion/countdown/index.js
@@ -1,5 +1,10 @@
 function countdown(n){
-
+    let arr = ["Liftoff!"];
+    for(let i = 1; i <= n; i++) {
+        arr.unshift(i);
+    }
+    return arr.join(', ');
 }
+console.log(countdown(9));
 
 module.exports = countdown
